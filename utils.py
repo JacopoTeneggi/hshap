74b2b7993fcd72d0b4eed19867f7b3bd207ca191
lustea0201 <alexandre.luster@epfl.ch>
2020-03-09 10:48:53 -0400

    FIxed shape issue

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def datasetMeanStd(loader):
     N = len(loader.dataset)
     for images, _ in loader:
         batch_samples = images.size(0)
-        images = images.view(batch_samples)
+        images = images.view(batch_samples, images.size(1), -1)
         mean += images.mean(2).sum(0)
         std += images.std(2).sum(0)
     mean /= N
