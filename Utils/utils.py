13d8364cd026ffd1c7e582499019e33c958c9f4e
lustea0201 <alexandre.luster@epfl.ch>
2020-07-03 13:05:18 +0200

    Commented utils.py

--- Utils/utils.py
+++ Utils/utils.py
@@ -5,7 +5,21 @@ import torch.nn.functional as F
 import numpy as np
 
 def datasetMeanStd(loader):
-    # Computes the mean and standard deviation of a DataLoader of 3 channel images
+    """ Computes the mean and standard deviation of a dataloader of 3 channel images
+
+    Parameters
+    ----------
+    loader : torch.utils.data.DataLoader
+        the dataloader whose mean is computed
+
+    Returns
+    -------
+    mean : numpy array of shape (3,)
+        the channel-wise mean of the dataloader
+    std : numpy array of shape (3,)
+        the channel-wise standard deviation of the dataloader
+    """
+
     mean = 0.
     std = 0.
     N = len(loader.dataset)
@@ -16,17 +30,69 @@ def datasetMeanStd(loader):
         std += images.std(2).sum(0)
     mean /= N
     std /= (N-1)
-    return mean.numpy(), std.numpy()
+    mean = mean.numpy()
+    std = std.numpy()
+    return mean, std
 
 
 def denormalize(im, mean, std):
-  return im*std + mean
+    """ Restore an image to its range before normalization
+
+    Parameters
+    ----------
+    im : numpy array with 3 channels
+        the image to denormalize
+    mean : numpy array of shape (3,)
+        the channel-wise mean of the dataloader
+    std : numpy array of shape (3,)
+        the channel-wise standard deviation of the dataloader
+
+    Returns
+    -------
+    denorm : numpy array with 3 channels
+        the denormalized image
+    """
+
+    denorm = im*std + mean
+    return denorm
 
 def input2image(input, mean, std):
-    sample_image = input.numpy().transpose(1, 2, 0)
-    return denormalize(sample_image, mean, std)
+    """ Convert an torch tensor input to a neural net as a image in numpy array format, denormalized.
 
-def display_image(im, true_label, predicted_label=None, figure_size = (8, 5)):
+    Parameters
+    ----------
+    input : torch tensor with 3 channels
+        the input tensor to process
+    mean : numpy array of shape (3,)
+        the channel-wise mean of the dataloader
+    std : numpy array of shape (3,)
+        the channel-wise standard deviation of the dataloader
+
+    Returns
+    -------
+    denorm : numpy array with 3 channels
+        the denormalized image
+    """
+
+    sample_image = input.numpy().transpose(1, 2, 0)
+    denorm = denormalize(sample_image, mean, std)
+    return deorm
+
+def display_image(im, true_label, predicted_label=None, figure_size = (8, 5))
+    """ Convert an torch tensor input to a neural net as a image in numpy array format, denormalized.
+
+    Parameters
+    ----------
+    im : numpy array with 3 channels
+        the image to display
+    true_label : int in {0,1}
+        the ground truth label of im
+    true_label : int in {0,1}, optional 
+        the predicted truth label of im by the neural network 
+    figure_size : tuple of ints, optional 
+        size of the matplotlib.pyplot.figure object
+    """
+
     plt.figure(figsize=figure_size)
     plt.imshow(im)
     title_ = "True label : " + str(true_label)
@@ -35,28 +101,98 @@ def display_image(im, true_label, predicted_label=None, figure_size = (8, 5)):
     plt.title(title_)
 
 def almost_equal(n1, n2, e):
-  return abs(n1-n2) < e
+    """ Determine if n1 and n2 are almost equal, at a tolerance e.
+
+    Parameters
+    ----------
+    n1 : float
+        the first number
+    n2 : float
+        the second number
+    e : float
+        the tolerance
+
+    Returns
+    -------
+    verdict : bool
+        whether or not they are almost equal
+    """
+
+    verdict = abs(n1-n2) < e
+    return verdict
 
 def network_has_converged(loss, e):
-  if (len(loss) < 3):
-    return False
-  else:
-    return (almost_equal(loss[-3], loss[-2], e) and
+    """ Determine if n1 and n2 are almost equal, at a tolerance e.
+
+    Parameters
+    ----------
+    loss : list of floats
+        the list of losses during training
+    e : float
+        the tolerance for the stopping criterion
+
+    Returns
+    -------
+    converged : bool
+        whether or not training has converged
+    """
+
+    if (len(loss) < 3):
+        converged = False
+    else:
+        converged = (almost_equal(loss[-3], loss[-2], e) and
             almost_equal(loss[-3], loss[-1], e) )
+    return converged
+
 
 def training_accuracy(network, loader):
-  with torch.no_grad():
-    correct = 0
-    total = 0
-    for data in loader:
-      images, labels = data
-      outputs = network(images)
-      _, predicted = torch.max(outputs.data, 1)
-      total += labels.size(0)
-      correct += (predicted == labels).sum().item()
-  return 100 * correct/total
+    """ Determine the accuracy of the predictions of a network.
+
+    Parameters
+    ----------
+    network : torch.nn.Module or subclass
+        the network to test
+    loader : torch.utils.data.DataLoader
+        the training dataloader
+
+    Returns
+    -------
+    accuracy : float betwee 0. and 100.
+        the training accuracy
+    """
+
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for data in loader:
+            images, labels = data
+            outputs = network(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = 100 * correct/total
+    return accuracy
 
 def validation_stats(network, loader, criterion):
+    """ Judge the training of the network on the validation set.
+
+    Parameters
+    ----------
+    network : torch.nn.Module or subclass
+        the network to test
+    loader : torch.utils.data.DataLoader
+        the validation dataloader
+    criterion : torch.nn loss function
+        the loss function criterion
+
+    Returns
+    -------
+    accuracy : float between 0. and 100.
+        the validation accuracy
+    normalized_loss : float
+        the loss per input
+    """
+
     total_loss = 0
     correct = 0
     total = 0
@@ -70,9 +206,24 @@ def validation_stats(network, loader, criterion):
           loss = criterion(outputs, labels)
           total_loss += loss.item()
 
-    return (100 * correct/total, total_loss/len(loader.dataset))
+    accuracy, normalized_loss = (100 * correct/total, total_loss/len(loader.dataset))
+    return accuracy, normalized_loss
 
 def plot_training(train_loss, val_loss, train_accuracy, val_accuracy):
+    """ Plot the training loss and accuracy of the network on the training and validation set.
+
+    Parameters
+    ----------
+    train_loss : list of floats
+        the loss on the training set
+    val_loss : list of floats
+        the loss on the validation set
+    train_accuracy : float between 0. and 100.
+        the accuracy on the training set
+    val_accuracy : float between 0. and 100.
+        the accuracy on the validations set
+    """
+
     fig, (ax1, ax2) = plt.subplots(1,2, figsize = (10,5))
     x_scale = np.linspace(0, len(train_loss)- 1, len(train_loss) )
     _ = ax1.plot(x_scale, train_loss)
@@ -90,6 +241,25 @@ def plot_training(train_loss, val_loss, train_accuracy, val_accuracy):
 
 
 def test(net, loader):
+    """ Confront the network to the testing set.
+
+    Parameters
+    ----------
+    net : torch.nn.Module or subclass
+        the network to test
+    loader : torch.utils.data.DataLoader
+        the testing dataloader
+
+    Returns
+    -------
+    wrong_im : list of torch tensors
+        inputs classified wrongly
+    wrong_label : list of ints ({0,1})
+        corresponding labels
+    wrong_label : list of ints ({0,1})
+        corresponding predictions (redundant)
+    """
+
     correct = 0
     total = 0
     wrong_im = []
@@ -120,6 +290,36 @@ def test(net, loader):
 
 
 def train(net, optimizer, criterion, max_epochs, dataloader, valloader ):
+    """ Train a CNN.
+
+    Parameters
+    ----------
+    net : torch.nn.Module or subclass
+        the network to test
+    optimizer : instance of a torch.optim class
+        the optimizer for the CNN
+    criterion : torch.nn loss function
+        the loss function criterion
+    max_epochs : int
+        maximum number of epochs before terminating training early
+    dataloader : torch.utils.data.DataLoader
+        dataloader for the training set
+    valloader : torch.utils.data.DataLoader
+        dataloader for the validation set
+
+
+    Returns
+    -------
+    train_loss : list of floats
+        the loss on the training set
+    val_loss : list of floats
+        the loss on the validation set
+    train_accuracy : float between 0. and 100.
+        the accuracy on the training set
+    val_accuracy : float between 0. and 100.
+        the accuracy on the validations set
+    """
+
     converged = False
     epsilon = 0.0001
     train_loss, val_loss, train_accuracy, val_accuracy = [], [], [], []
@@ -165,8 +365,13 @@ def train(net, optimizer, criterion, max_epochs, dataloader, valloader ):
     return train_loss, val_loss, train_accuracy, val_accuracy
 
 class Net(nn.Module):
+    """ CNN architecture for classifying images of 120 pixels in width and 100 in height.
+    """
 
     def __init__(self):
+        """ Initialize the CNN.
+        """
+
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool1 = nn.MaxPool2d(2)
@@ -178,6 +383,19 @@ class Net(nn.Module):
         self.drop = nn.Dropout(p=0.5)
 
     def forward(self, x):
+        """ Perform forward propagation across the network.
+
+        Parameters
+        ----------
+        x : torch tensor
+            CNN input tensor images
+
+        Returns
+        -------
+        x : torch tensor
+            CNN output class labels
+        """
+
         x = F.relu(self.conv1(x))
         x = self.pool1(x)
         x = F.relu(self.conv2(x))
@@ -189,6 +407,19 @@ class Net(nn.Module):
         return x
 
     def num_flat_features(self, x):
+        """ Compute the number entries of one item in a batch.
+
+        Parameters
+        ----------
+        x : torch tensor
+            batch of inputs or processed inputs
+
+        Returns
+        -------
+        num_features : int
+            number of entries in one item
+        """
+
         size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
@@ -197,7 +428,12 @@ class Net(nn.Module):
 
 
 class HdNet(nn.Module):
+    """ CNN architecture for classifying images of 1200 pixels in width and 1000 in height.
+        """
     def __init__(self):
+        """ Initialize the CNN.
+        """
+
         super(HdNet, self).__init__()
         self.conv0 = nn.Conv2d(in_channels = 3, out_channels = 6, kernel_size = 5, stride=5)
         self.pool0 = nn.MaxPool2d(kernel_size = 2, stride=2)
@@ -211,6 +447,19 @@ class HdNet(nn.Module):
         self.drop = nn.Dropout(p=0.5)
 
     def forward(self, x):
+        """ Perform forward propagation across the network.
+
+        Parameters
+        ----------
+        x : torch tensor
+            CNN input tensor images
+
+        Returns
+        -------
+        x : torch tensor
+            CNN output class labels
+        """
+
         x = F.relu(self.conv0(x))
         x = self.pool0(x)
         x = F.relu(self.conv1(x))
@@ -224,6 +473,19 @@ class HdNet(nn.Module):
         return x
 
     def num_flat_features(self, x):
+        """ Compute the number entries of one item in a batch.
+
+        Parameters
+        ----------
+        x : torch tensor
+            batch of inputs or processed inputs
+
+        Returns
+        -------
+        num_features : int
+            number of entries in one item
+        """
+
         size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
